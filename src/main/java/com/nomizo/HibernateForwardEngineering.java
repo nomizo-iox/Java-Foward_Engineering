31f8d0431bf112ad519a3688874fb6376180e059
nomizo-iox <nomizo.iox@gmail.com>
2020-06-06 12:37:12 -0500

    2nd commit.

--- src/main/java/com/nomizo/HibernateForwardEngineering.java
+++ src/main/java/com/nomizo/HibernateForwardEngineering.java
@@ -1,11 +1,14 @@
 package com.nomizo;
 
+import com.nomizo.model.Product;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.security.auth.login.Configuration;
 
 public class HibernateForwardEngineering {
     public static void main(String[] args) {
@@ -14,17 +17,23 @@ public class HibernateForwardEngineering {
                 .configure()
                 .build();
 
-        SessionFactory factory = new MetadataSources(registry)
-                .buildMetadata()
-                .buildSessionFactory();
+//        SessionFactory factory = new MetadataSources(registry)
+//                .buildMetadata()
+//                .buildSessionFactory();
 
-        Session session = factory
-                .openSession();
+        SessionFactory factory new Configuration().co
 
-        session.beginTransaction();
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
 
-        Transaction transaction = session
-                .beginTransaction();
+        Product product = new Product();
+        product.setName("iPhone 10");
+        product.setPrice(999.88f);
+
+        session.save(product);
+        transaction.commit();
+        session.close();
+        factory.close();
     }
 
 
